052167c6cd7c2b0773d2fb41d6444cb720e3c937
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2026-01-02 21:02:17 +0000

    Fix catch-all route for production deployment

--- server/server.js
+++ server/server.js
@@ -341,7 +341,7 @@ app.get("/events", (req, res) => {
 
 // Serve React app for all other routes in production
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/hybrid-client/build/index.html'));
   });
 }
